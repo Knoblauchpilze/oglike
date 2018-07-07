107f88ea6713efd80289e0208c26a3eb6a74e403
MaximeMYCS <prolific-programmer@tuta.com>
2018-07-07 16:51:05 +0200

    Updated ogame view to provide the general layout of the final application

--- src/gui/OgameView.h
+++ src/gui/OgameView.h
@@ -5,9 +5,10 @@
 #include <unordered_map>
 #include <SDL/SDL.h>
 #include "OgameAbstractView.h"
-#include "LinearLayout.h"
 #include "System.h"
+#include "GraphicContainer.h"
 #include "GalaxyView.h"
+#include "SelectorPanel.h"
 
 namespace ogame {
   namespace gui {
@@ -30,18 +31,37 @@ namespace ogame {
 
           void initializeTTFLib();
 
+          void createMainPanel(const float& margin);
+
           void createViews(const float& margin,
                            const unsigned& galaxyCount,
                            const unsigned& systemCount,
                            const unsigned& planetCount);
 
-          void createGalaxyView(const unsigned& galaxyCount,
-                                const unsigned& systemCount,
-                                const unsigned& planetCount);
+          void createMainLayout();
+
+          view::GraphicContainerShPtr createGraphicContainer(const std::string& name) const;
+
+          void createThematicViews(const unsigned& galaxyCount,
+                                   const unsigned& systemCount,
+                                   const unsigned& planetCount);
 
       private:
 
+        // Main panel.
         view::GraphicContainerShPtr m_panel;
+        // Left view (i.e. toggle between main views)
+        view::GraphicContainerShPtr m_menuView;
+        // Upper view to access highscores, options...)
+        view::GraphicContainerShPtr m_optionsView;
+        // Upper view to display the resource of the current planet
+        view::GraphicContainerShPtr m_resourcesView;
+        // The list of the planets for this user.
+        view::GraphicContainerShPtr m_planetsView;
+        // The main view displayed.
+        SelectorPanelShPtr m_generalView;
+
+        // Galaxy view display.
         GalaxyViewShPtr m_galaxyView;
 
     };
--- src/gui/OgameView.hxx
+++ src/gui/OgameView.hxx
@@ -3,6 +3,7 @@
 
 #include "OgameView.h"
 #include <SDL/SDL_ttf.h>
+#include "GuiException.h"
 
 namespace ogame {
   namespace gui {
@@ -18,6 +19,39 @@ namespace ogame {
       if (!TTF_WasInit() && TTF_Init() == -1) {
         throw GuiException(std::string("Unable to initialize TTF lib, cannot create view: ") + TTF_GetError());
       }
+    }
+
+    inline
+    void OgameView::createMainPanel(const float& margin) {
+      // Create the main panel in which views will be displayed.
+      m_panel = std::make_shared<view::GraphicContainer>(
+        std::string("Panel 0"),
+        view::utils::Area(margin,
+                          margin,
+                          static_cast<float>(getWidth()) - 2.0f * margin,
+                          static_cast<float>(getHeight()) - 2.0f * margin),
+        view::EventListener::Interaction::MouseButton
+      );
+
+      // Check that the creation was successful.
+      if (m_panel == nullptr) {
+        throw GuiException(std::string("Could not create main panel used for display"));
+      }
+
+      // Add main panel to drawables.
+      addDrawable(m_panel.get());
+
+      // Add main panel to listeners.
+      addListener(m_panel.get());
+    }
+
+    inline
+    view::GraphicContainerShPtr OgameView::createGraphicContainer(const std::string& name) const {
+      return std::make_shared<view::GraphicContainer>(
+        name,
+        view::utils::Area(),
+        view::EventListener::Interaction::NoInteraction
+      );
     }
 
   }
