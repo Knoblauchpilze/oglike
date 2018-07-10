31357bf3d4072ff80f9a4596ea80fd2f6cea9756
MaximeMYCS <prolific-programmer@tuta.com>
2018-07-10 20:40:10 +0200

    Added state container method to notify inheriting classes that the state has been modified

--- src/gui/StateContainer.hxx
+++ src/gui/StateContainer.hxx
@@ -1,13 +1,19 @@
 #ifndef STATE_CONTAINER_HXX
 #define STATE_CONTAINER_HXX
 
-#include <memory>
-#include <unordered_map>
-#include <SDL/SDL.h>
-#include "GraphicContainer.h"
+#include "StateContainer.h"
+#include "GuiException.h"
 
 namespace ogame {
   namespace gui {
+
+    inline
+    const StateContainer::State StateContainer::getState() {
+      lock();
+      State state = getStatePrivate();
+      unlock();
+      return state;
+    }
 
     inline
     void StateContainer::onMouseMotionEventPrivate(const SDL_MouseMotionEvent& mouseMotionEvent) {
@@ -15,36 +21,36 @@ namespace ogame {
       const float x = static_cast<float>(mouseMotionEvent.x);
       const float y = static_cast<float>(mouseMotionEvent.y);
 
-      std::cout << "-----" << std::endl;
+      // std::cout << "-----" << std::endl;
       if (isInside(x, y) && m_state != State::Selected) {
         if (m_state != State::Highlighted) {
-          std::cout << "[STATE] " << getName()
-                    << " mouse: " << x << "x" << y
-                    << ", area=" << getRenderingAreaPrivate().x() << "x" << getRenderingAreaPrivate().y()
-                    << " dims=" << getRenderingAreaPrivate().w() << "x" << getRenderingAreaPrivate().h()
-                    << ", state was " << (m_state == State::Selected ? "selected" : m_state == State::Highlighted ? "highlighted": "normal")
-                    << " and is now highlighted"
-                    << std::endl;
+          // std::cout << "[STATE] " << getName()
+          //           << " mouse: " << x << "x" << y
+          //           << ", area=" << getRenderingAreaPrivate().x() << "x" << getRenderingAreaPrivate().y()
+          //           << " dims=" << getRenderingAreaPrivate().w() << "x" << getRenderingAreaPrivate().h()
+          //           << ", state was " << (m_state == State::Selected ? "selected" : m_state == State::Highlighted ? "highlighted": "normal")
+          //           << " and is now highlighted"
+          //           << std::endl;
           m_state = State::Highlighted;
-          setBackgroundColor(getColorFromState(m_state));
+          assignColorFromState(m_state);
         }
       }
-      std::cout << "-----" << std::endl;
-      std::cout << "*****" << std::endl;
+      // std::cout << "-----" << std::endl;
+      // std::cout << "*****" << std::endl;
       if (!isInside(x, y) && m_state != State::Normal) {
         if (m_state != State::Selected) {
-          std::cout << "[STATE] " << getName()
-                    << " mouse: " << x << "x" << y
-                    << ", area=" << getRenderingAreaPrivate().x() << "x" << getRenderingAreaPrivate().y()
-                    << " dims=" << getRenderingAreaPrivate().w() << "x" << getRenderingAreaPrivate().h()
-                    << ", state was " << (m_state == State::Selected ? "selected" : m_state == State::Highlighted ? "highlighted": "normal")
-                    << " and is now normal"
-                    << std::endl;
+          // std::cout << "[STATE] " << getName()
+          //           << " mouse: " << x << "x" << y
+          //           << ", area=" << getRenderingAreaPrivate().x() << "x" << getRenderingAreaPrivate().y()
+          //           << " dims=" << getRenderingAreaPrivate().w() << "x" << getRenderingAreaPrivate().h()
+          //           << ", state was " << (m_state == State::Selected ? "selected" : m_state == State::Highlighted ? "highlighted": "normal")
+          //           << " and is now normal"
+          //           << std::endl;
           m_state = State::Normal;
-          setBackgroundColor(getColorFromState(m_state));
+          assignColorFromState(m_state);
         }
       }
-      std::cout << "*****" << std::endl;
+      // std::cout << "*****" << std::endl;
     }
 
     inline
@@ -55,25 +61,56 @@ namespace ogame {
 
       if (isInside(x, y) && m_state != State::Selected) {
         m_state = State::Selected;
-        setBackgroundColor(getColorFromState(m_state));
+        assignColorFromState(m_state);
       }
       if (!isInside(x, y) && m_state != State::Normal) {
         m_state = State::Normal;
-        setBackgroundColor(getColorFromState(m_state));
+        assignColorFromState(m_state);
       }
     }
 
     inline
+    void StateContainer::onStateModified() {
+      // Nothing to do here.
+    }
+
+    inline
+    const StateContainer::State& StateContainer::getStatePrivate() const noexcept {
+      return m_state;
+    }
+
+    inline
     const SDL_Color StateContainer::getColorFromState(const State& state) const noexcept {
-      unsigned index = 0u;
-      while (index < m_colors.size()) {
-        if (m_colors[index].first == state) {
-          return m_colors[index].second;
-        }
-        ++index;
+      const StateAssociation::const_iterator color = m_colors.find(state);
+      if (color != m_colors.cend()) {
+          return color->second;
       }
 
-      return SDL_Color{0, 0, 0, SDL_ALPHA_OPAQUE};
+      // Could not find a color for this state.
+      throw GuiException(std::string("Could not find color for state in container ") + getName());
+    }
+
+    inline
+    void StateContainer::assignColorFromState(const State& state) {
+      try {
+        SDL_Color color = getColorFromState(state);
+        setBackgroundColor(color);
+      }
+      catch (const GuiException& e) {
+        // Handle according to the policy.
+        if (m_policy == FailPolicy::Aggressive) {
+          assignNormalStateOrDefault();
+        }
+      }
+    }
+
+    inline
+    void StateContainer::assignNormalStateOrDefault() {
+      // Search for the normal state.
+      const StateAssociation::const_iterator color = m_colors.find(State::Normal);
+      if (color != m_colors.cend()) {
+        setBackgroundColor(color->second);
+      }
     }
 
   }
