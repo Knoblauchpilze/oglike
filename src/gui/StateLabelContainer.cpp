53575f542f081852139bd6b4ed5b760aff521ddc
MaximeMYCS <maxime.rigaud.perso@gmail.com>
2018-07-10 20:38:33 +0200

    Added implementation of state label container which inherits from state container and describe a real highlightable label container

---
+++ src/gui/StateLabelContainer.cpp
@@ -0,0 +1,71 @@
+
+#include "StateLabelContainer.h"
+#include "GuiException.h"
+
+namespace ogame {
+  namespace gui {
+
+    StateLabelContainer::StateLabelContainer(const std::string& name,
+                                             const std::string& text,
+                                             view::ColoredFontShPtr font,
+                                             view::ColoredFontShPtr highlightFont,
+                                             const StateContainer::StateAssociation& colors,
+                                             const StateContainer::FailPolicy& policy):
+      StateContainer(name,
+                     StateContainer::State::Normal,
+                     colors,
+                     policy),
+      m_textChanged(true),
+      m_text(text),
+      
+      m_fontChanged(true),
+      m_font(font),
+      
+      m_hFontChanged(true),
+      m_hFont(highlightFont),
+
+      m_textSurface(nullptr)
+    {
+      // Nothing to do here.
+    }
+
+    StateLabelContainer::~StateLabelContainer() {
+      clearText();
+    }
+
+    SDL_Surface* StateLabelContainer::createContentPrivate() {
+      // Create the text to render.
+      createText();
+
+      // Create the surface based on the dimensions of this container.
+      SDL_Surface* croppedArea = StateContainer::createContentPrivate();
+      if (croppedArea == nullptr) {
+        throw GuiException(std::string("Unable to create perform rendering for state label container ") + getName() + " using text " + m_text);
+      }
+      StateContainer::clearContentPrivate(croppedArea);
+
+      // Check whether some text should be displayed.
+      if (m_textSurface != nullptr) {
+        // Compute the blit position of the picture so that it is centered.
+        SDL_Rect textBlitArea = {
+          static_cast<Sint16>(croppedArea->w / 2.0f - m_textSurface->w / 2.0f),
+          static_cast<Sint16>(croppedArea->h / 2.0f - m_textSurface->h / 2.0f),
+          static_cast<Uint16>(m_textSurface->w),
+          static_cast<Uint16>(m_textSurface->h)
+        };
+
+        // Perform the blit operation (i.e. stick the text onto the rendering area).
+        SDL_BlitSurface(m_textSurface, nullptr, croppedArea, &textBlitArea);
+      }
+
+      // Reset flags.
+      m_fontChanged = false;
+      m_hFontChanged = false;
+      m_textChanged = false;
+
+      // This is the final image for this container.
+      return croppedArea;
+    }
+
+  }
+}
