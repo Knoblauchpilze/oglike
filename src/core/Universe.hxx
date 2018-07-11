0bd1afee450220f2c07568c7a6eb44ab1284a564
MaximeMYCS <prolific-programmer@tuta.com>
2018-07-11 21:37:21 +0200

    Updated universe and planet classes to use new community and player and account class

--- src/core/Universe.hxx
+++ src/core/Universe.hxx
@@ -7,6 +7,21 @@ namespace ogame {
   namespace core {
 
     inline
+    bool Universe::operator==(const Universe& other) const {
+      return m_index == other.m_index;
+    }
+
+    inline
+    bool Universe::operator<(const Universe& other) const {
+      return m_index < other.m_index;
+    }
+
+    inline
+    const unsigned& Universe::getUuid() const noexcept {
+      return m_index;
+    }
+
+    inline
     const bool Universe::checkForDuplicatedAccounts(const Account& account) const {
       for (unsigned indexAccount = 0u ; indexAccount < m_accounts.size() ; ++indexAccount) {
         if (*m_accounts[indexAccount] == account) {
