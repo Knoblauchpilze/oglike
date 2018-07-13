b5130092941048f2343628fb90eb344fbe6fa4da
MaximeMYCS <prolific-programmer@tuta.com>
2018-07-13 22:29:12 +0200

    Added a lot of information in account class

--- src/core/Account.h
+++ src/core/Account.h
@@ -3,9 +3,12 @@
 
 #include <memory>
 #include "Community.h"
+#include "Planet.h"
 
 namespace ogame {
   namespace core {
+
+    class Planet;
 
     class Account
     {
@@ -29,11 +32,32 @@ namespace ogame {
 
         CommunityShPtr getCommunity() const noexcept;
 
+        const float getAntimatterQuantity() const noexcept;
+
+        const unsigned& getAvailablePlanetsSlots() const noexcept;
+        
+        const unsigned& getOccupiedPlanetsSlots() const noexcept;
+
+        void incrementAvailablePlanetsSlots() noexcept;
+
+        const core::Planet& getHomeWorld() const;
+
+        core::Planet& getHomeWorld();
+
+        void setHomePlanet(core::Planet* planet);
+
+        const std::vector<core::Planet*>& getPlanets() const noexcept;
+
       private:
 
         CommunityShPtr m_community;
         unsigned m_playerUuid;
         unsigned m_universeUuid;
+        float m_antimatter;
+        
+        unsigned m_availablePlanetsSlots;
+        unsigned m_occupiedPlanetsSlots;
+        std::vector<core::Planet*> m_planets;
 
     };
 
