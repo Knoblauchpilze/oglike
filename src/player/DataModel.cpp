bd35c983a6ea37345d759b231fae424ed9006032
MaximeMYCS <maxime.rigaud.perso@gmail.com>
2018-07-13 22:27:41 +0200

    Added attributes to data model class and added actual triggering of actions

--- src/player/DataModel.cpp
+++ src/player/DataModel.cpp
@@ -5,15 +5,24 @@ namespace ogame {
   namespace player {
 
     DataModel::DataModel(const View& initView):
-      m_activeView(initView),
-      m_listeners()
+      m_listeners(),
+      m_activeAccount(nullptr),
+      m_activePlanet(nullptr),
+      m_activeView(initView)
     {
       // Nothing to do here.
     }
 
     DataModel::~DataModel() {}
 
-    void DataModel::triggerActiveViewModification(const View& view) {}
+    void DataModel::triggerAction(const ActionListener::Action& action) {
+      for (unsigned listenerIndex = 0u ; listenerIndex < m_listeners.size() ; ++listenerIndex) {
+        ActionListener* listener = m_listeners[listenerIndex];
+        if (listener != nullptr && listener->getAction() == action) {
+          listener->onActionTriggered(*this);
+        }
+      }
+    }
 
   }
 }
--- src/player/DataModel.h
+++ src/player/DataModel.h
@@ -4,9 +4,13 @@
 #include <memory>
 #include <vector>
 #include "ActionListener.h"
+#include "Account.h"
+#include "Planet.h"
 
 namespace ogame {
   namespace player {
+
+    class ActionListener;
 
     class DataModel
     {
@@ -28,14 +32,28 @@ namespace ogame {
 
         ~DataModel();
 
-        void registerForAction(const ActionListener::Action& action, ActionListenerShPtr listener);
+        void registerForAction(const ActionListener::Action& action, ActionListener* listener);
 
-        void triggerActiveViewModification(const View& view);
+        void triggerAction(const ActionListener::Action& action);
+
+        void setActiveAccount(core::Account* account);
+
+        void setActivePlanet(core::Planet* planet);
+
+        void setActiveView(const View& view);
+
+        const core::Account& getActiveAccount() const;
+
+        const core::Planet& getActivePlanet() const;
+
+        const View& getActiveView() const;
 
       private:
 
+        std::vector<ActionListener*> m_listeners;
+        core::Account* m_activeAccount;
+        core::Planet* m_activePlanet;
         View m_activeView;
-        std::vector<ActionListenerShPtr> m_listeners;
 
     };
 
