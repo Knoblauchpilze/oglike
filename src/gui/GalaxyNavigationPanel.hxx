801c94539966e0eefe8df4c04cdc00f7db973e34
MaximeMYCS <maxime.rigaud.perso@gmail.com>
2018-07-19 22:02:43 +0200

    Galaxy navigation panel is now connected to value selector modification to update containers

--- src/gui/GalaxyNavigationPanel.hxx
+++ src/gui/GalaxyNavigationPanel.hxx
@@ -10,11 +10,36 @@ namespace ogame {
     inline
     void GalaxyNavigationPanel::onActionTriggered(const player::GeneralDataModel& model) {
       try {
-        const core::System* activeSystem = model.getProperty<const core::System>(std::string("active_system"));
-        populateWithSystemData(*activeSystem);
+        const unsigned& galaxyCoord = model.getActiveGalaxyCoordinate();
+        const unsigned& systemCoord = model.getActiveSystemCoordinate();
+        const core::Account& account = model.getActiveAccount();
+        const core::System& activeSystem = account.getSystem(core::SystemCoordinates(galaxyCoord, systemCoord));
+        populateWithSystemData(activeSystem);
       }
       catch (const player::DataModelException& e) {
-        std::cerr << "[NAVIGATIOn] Caught exception while setting up galaxy navigation panel:" << std::endl << e.what() << std::endl;
+        std::cerr << "[NAVIGATION] Caught exception while setting up galaxy navigation panel:" << std::endl << e.what() << std::endl;
+      }
+    }
+
+    inline
+    void GalaxyNavigationPanel::onInteractionPerformed(const std::string& origin, const view::EventListener::Interaction::Mask& mask) {
+      // Determine the origin of the event (either galaxy or system selector) and update the corresponding value in the data model.
+      if (origin == std::string("galaxy_selector")) {
+        // Retrieve the value from the galaxy selector
+        ValueSelector* galaxySelector = getChild<ValueSelector*>(std::string("galaxy_selector"));
+        if (checkChild(galaxySelector, std::string("Galaxy selector"))) {
+          setActiveGalaxyCoordinate(galaxySelector->getActiveOption());
+        }
+      }
+      else if (origin == std::string("system_selector")) {
+        // Retrieve the value from the galaxy selector
+        ValueSelector* systemSelector = getChild<ValueSelector*>(std::string("system_selector"));
+        if (checkChild(systemSelector, std::string("System selector"))) {
+          setActiveSystemCoordinate(systemSelector->getActiveOption());
+        }
+      }
+      else {
+        std::cerr << "[NAVIGATION] Caught interaction in " << getName() << " from unknown origin " << origin << std::endl;
       }
     }
 
