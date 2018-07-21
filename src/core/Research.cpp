4542179e93b500a9fd349e326255ab2b92249a83
MaximeMYCS <maxime.rigaud.perso@gmail.com>
2018-07-21 22:15:48 +0200

    Added first dummy implementation for research

---
+++ src/core/Research.cpp
@@ -0,0 +1,16 @@
+
+#include "Research.h"
+
+namespace ogame {
+  namespace core {
+
+    Research::Research(const std::string& name):
+      m_name(name)
+    {
+      // Nothing to do here.
+    }
+
+    Research::~Research() {}
+
+  }
+}
---
+++ src/core/Research.hxx
@@ -0,0 +1,17 @@
+#ifndef RESEARCH_HXX
+#define RESEARCH_HXX
+
+#include "Research.h"
+
+namespace ogame {
+  namespace core {
+
+    inline
+    const std::string& Research::getName() const {
+      return m_name;
+    }
+
+  }
+}
+
+#endif // RESEARCH_HXX
