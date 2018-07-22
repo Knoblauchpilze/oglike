3ed413aeac2fec51944d68cf481c9d22e75e9a69
MaximeMYCS <prolific-programmer@tuta.com>
2018-07-22 10:39:13 +0200

    Updated buildings view to rely on the building class to update the data displayed. Implementation is partial but better than before.

--- src/gui/BuildingsView.hxx
+++ src/gui/BuildingsView.hxx
@@ -2,44 +2,13 @@
 #define BUILDINGSVIEW_HXX
 
 #include "BuildingsView.h"
-#include "PlanetException.h"
-#include "DataModelException.h"
-#include "ComponentFactory.h"
-#include "FontFactory.h"
 
 namespace ogame {
   namespace gui {
 
     inline
-    void BuildingsView::onActionTriggered(const player::GeneralDataModel& model) {
-      // Process this action.
-      try {
-        const core::Planet& planet = model.getActivePlanet();
-        populateWithPlanetData(planet);
-      }
-      catch (const core::PlanetException& e) {
-        std::cerr << "[BUILDINGS] Caught exception while setting up buildings view:" << std::endl << e.what() << std::endl;
-      }
-      catch (const player::DataModelException& e) {
-        std::cerr << "[BUILDINGS] Caught exception while setting up buildings view:" << std::endl << e.what() << std::endl;
-      }
-    }
-
-    inline
-    LabelledPictureShPtr BuildingsView::createLabelledPictureContainer(const std::string& name,
-                                                                       const std::string& picture,
-                                                                       const std::string& text) const
-    {
-      return ComponentFactory::createLabelledPicturePanel(
-        name,
-        view::FontFactory::getInstance().createColoredFont(
-          std::string("data/fonts/upcfb.ttf"),
-          219, 140, 40, SDL_ALPHA_OPAQUE
-        ),
-        picture,
-        text,
-        LabelledPicture::Alignment::Above
-      );
+    const std::string BuildingsView::getDisplayForBuilding(const core::Building::Type& type, const core::Planet& planet) const noexcept {
+      return std::to_string(0);
     }
 
   }
