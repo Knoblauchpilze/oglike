b8e62d50464511c01792821f210cfee7de3bd96e
MaximeMYCS <prolific-programmer@tuta.com>
2018-07-22 10:01:13 +0200

    Renamed and updated building so that it can be used in a more abstract way to 'upgrade' objects and not only buildings

---
+++ src/core/Upgradable.cpp
@@ -0,0 +1,16 @@
+
+#include "Upgradable.h"
+
+namespace ogame {
+  namespace core {
+
+    Upgradable::Upgradable(const unsigned& initLevel):
+      m_level(initLevel)
+    {
+      // Nothing to do here.
+    }
+
+    Upgradable::~Upgradable() {}
+
+  }
+}
