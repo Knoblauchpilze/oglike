3a7eb105af404f8aff82eb17347badaf226207d8
MaximeMYCS <maxime.rigaud.perso@gmail.com>
2018-07-22 16:02:45 +0200

    Added partial implementation for buildings buying action

--- src/gui/BuildingsView.hxx
+++ src/gui/BuildingsView.hxx
@@ -7,6 +7,11 @@ namespace ogame {
   namespace gui {
 
     inline
+    void BuildingsView::populateActionData(const std::string& origin, ElementDescriptionPanel& action) const {
+      std::cout << "[BUILDINGS] Should create description for building" <<  std::endl;
+    }
+
+    inline
     const std::string BuildingsView::getDisplayForBuilding(const core::Building::Type& type, const core::Planet& planet) const noexcept {
       return std::to_string(0);
     }
