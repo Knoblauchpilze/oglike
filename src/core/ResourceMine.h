fa86d38bb1d0dbcaa9188c9270f7b9cb47a08aa0
MaximeMYCS <prolific-programmer@tuta.com>
2018-07-22 10:01:39 +0200

    Resource mine now inherits from upgradable

--- src/core/ResourceMine.h
+++ src/core/ResourceMine.h
@@ -2,22 +2,20 @@
 #define RESOURCEMINE_H
 
 #include "Resource.h"
-#include "Building.h"
+#include "Upgradable.h"
 
 namespace ogame {
   namespace core {
 
-    class ResourceMine: public Building
+    class ResourceMine: public Upgradable
     {
       public:
+
+        ResourceMine(const Resource& resource, const float& factor);
 
         virtual ~ResourceMine();
 
         void upgrade(const std::vector<ResourceDepositShPtr>& resources) override;
-
-      protected:
-
-        ResourceMine(const Resource& resource, const float& factor);
 
       private:
 
