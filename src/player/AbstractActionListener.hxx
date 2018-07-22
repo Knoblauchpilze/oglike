6cc8299184dce5294359f2c576ae223f1d856071
MaximeMYCS <prolific-programmer@tuta.com>
2018-07-22 11:32:40 +0200

    Added utility method to retrieve the data model associated to an abstract action listener

---
+++ src/player/AbstractActionListener.hxx
@@ -0,0 +1,21 @@
+#ifndef ABSTRACTACTIONLISTENER_HXX
+#define ABSTRACTACTIONLISTENER_HXX
+
+#include "AbstractActionListener.h"
+#include "DataModelException.h"
+
+namespace ogame {
+  namespace player {
+
+    inline
+    AbstractDataModel& AbstractActionListener::getDataModel() {
+      if (m_model == nullptr) {
+        throw DataModelException(std::string("Cannot retrieve data model, no model associated to this action listener"));
+      }
+      return *m_model;
+    }
+
+  }
+}
+
+#endif // ABSTRACTACTIONLISTENER_HXX
