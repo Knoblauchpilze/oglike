8ed1e0e405a28d846befba394653cc3d9088d811
MaximeMYCS <maxime.rigaud.perso@gmail.com>
2018-07-25 22:30:45 +0200

    Added more complete implementation for upgradable class. It now provides some method to get the upgrade cost and the upgrade time according to the level

--- src/core/Upgradable.h
+++ src/core/Upgradable.h
@@ -3,6 +3,8 @@
 
 #include <memory>
 #include <vector>
+#include <unordered_map>
+#include "Resource.h"
 #include "ResourceDeposit.h"
 
 namespace ogame {
@@ -16,20 +18,30 @@ namespace ogame {
 
         const unsigned& getLevel() const noexcept;
 
-        virtual void upgrade(const std::vector<ResourceDepositShPtr>& resources) = 0;
+        virtual void upgrade(std::vector<ResourceDepositShPtr>& resources) = 0;
 
         // Time in seconds.
-        const float getUpgradeTime() const;
+        const float getUpgradeTime(const unsigned& laboratoryLevel) const;
+
+        const std::unordered_map<Resource, float>& getUpgradeCost() const;
+
+        const bool canUpgrade(const std::vector<ResourceDepositShPtr>& resources) const;
 
       protected:
 
         Upgradable() = delete;
 
-        Upgradable(const unsigned& initLevel);
+        Upgradable(const unsigned& initLevel,
+                   const std::unordered_map<Resource, float>& initCost);
 
       private:
 
+        void computeUpgradeDuration();
+
+      protected:
+
         unsigned m_level;
+        std::unordered_map<Resource, float> m_upgradeCost;
     };
 
     using UpgradableShPtr = std::shared_ptr<Upgradable>;
