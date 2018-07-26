4c1aeb88e9f263e5d5a3314a0c3b5368955d4320
MaximeMYCS <prolific-programmer@tuta.com>
2018-07-26 21:21:11 +0200

    Refactored building to remove unnecessary classes

--- src/core/Building.hxx
+++ src/core/Building.hxx
@@ -2,6 +2,7 @@
 #define BUILDING_HXX
 
 #include "Building.h"
+#include <cmath>
 
 namespace ogame {
   namespace core {
@@ -14,6 +15,20 @@ namespace ogame {
     inline
     const std::string& Building::getName() const noexcept {
       return m_name;
+    }
+
+    inline
+    const float Building::getUpgradeTime(const unsigned& universeSpeed,
+                                         const unsigned& roboticsLevel,
+                                         const unsigned& naniteLevel) const
+    {
+      const std::unordered_map<Resource, float>::const_iterator metal = m_upgradeCost.find(Resource(std::string("metal")));
+      const std::unordered_map<Resource, float>::const_iterator crystal = m_upgradeCost.find(Resource(std::string("crystal")));
+
+      const float metalValue = (metal != m_upgradeCost.cend() ? metal->second : 0.0f);
+      const float crystalValue = (crystal != m_upgradeCost.cend() ? crystal->second : 0.0f);
+
+      return std::max(1.0f, 3600.0f * (metalValue + crystalValue) / (universeSpeed * 2500.0f * (1.0f + roboticsLevel) * std::pow(2.0f, 1.0f * naniteLevel)));
     }
 
   }
