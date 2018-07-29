3c76a46d6712a4095bab6d01d148b6d6a8cd5b5f
MaximeMYCS <prolific-programmer@tuta.com>
2018-07-29 10:07:26 +0200

    Added pure virtual method in upgrade to retrieve information on the upgrade

--- src/core/UpgradeAction.h
+++ src/core/UpgradeAction.h
@@ -20,6 +20,10 @@ namespace ogame {
         // Expressed in seconds.
         const float getRemainingTime() const;
 
+        virtual const std::string getName() const = 0;
+
+        virtual const std::string getDescription() const = 0;
+
       protected:
 
         enum class Type {
