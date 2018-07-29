4fdd945686a34771d35b95a054bc306afd1af0bc
MaximeMYCS <maxime.rigaud.perso@gmail.com>
2018-07-29 12:02:23 +0200

    Updated upgrade action container

--- src/gui/UpgradeActionContainer.cpp
+++ src/gui/UpgradeActionContainer.cpp
@@ -9,9 +9,12 @@ namespace ogame {
   namespace gui {
 
     UpgradeActionContainer::UpgradeActionContainer(const std::string& name):
-      view::GraphicContainer(name,
-                             view::utils::Area())
+      SelectorPanel(name,
+                    view::EventListener::Interaction::NoInteraction,
+                    false)
     {
+      setBackgroundColor(SDL_Color{28, 33, 38, SDL_ALPHA_OPAQUE});
+
       createView();
     }
 
@@ -20,9 +23,7 @@ namespace ogame {
     void UpgradeActionContainer::populateWithBuildingData(const core::Planet& planet) {
       lock();
 
-      // Update each information.
       try {
-
         // Retrieve the building upgrades action and display the first one.
         const std::vector<core::BuildingUpgradeActionShPtr>& upgrades = planet.getBuildingUpgrades();
         bool hasBuildingUpgrade = !upgrades.empty();
@@ -31,48 +32,22 @@ namespace ogame {
           upgrade = upgrades[0u].get();
         }
 
+        std::cout << "[UPGRADE] Planet " << planet.getName() << " has " << upgrades.size() << " building(s) upgrade(s)" << std::endl;
 
-        LabelContainer* title = getChild<LabelContainer*>(std::string("title"));
-        if (checkChild(title, "Upgrade action container title")) {
-          title->setText("Buildings");
-        }
-
-        LabelContainer* upgradeName = getChild<LabelContainer*>(std::string("upgrade_name"));
-        if (checkChild(upgradeName, "Upgrade action container upgrade name")) {
-          upgradeName->setText(hasBuildingUpgrade ?
-            upgrade->getName() :
-            std::string("")
-          );
-        }
-
-        PictureContainer* picture = getChild<PictureContainer*>(std::string("picture"));
-        if (checkChild(picture, "Upgrade action container picture")) {
-          if (hasBuildingUpgrade) {
-            picture->setImagePath(getPictureNameFromBuilding(upgrade->getType()));
-          }
-          else {
-            picture->clear();
-          }
-        }
-
-        LabelContainer* upgradeDesc = getChild<LabelContainer*>(std::string("upgrade_desc"));
-        if (checkChild(upgradeDesc, "Upgrade action container description")) {
-          upgradeDesc->setText(hasBuildingUpgrade ?
-            upgrade->getDescription() :
-            std::string("")
-          );
-        }
-
-        LabelContainer* duration = getChild<LabelContainer*>(std::string("duration_value"));
-        if (checkChild(duration, "Upgrade action container duration")) {
-          duration->setText("tata");
-          duration->setText(hasBuildingUpgrade ?
-            (upgrade->isFinished() ?
+        if (hasBuildingUpgrade) {
+          populatePanel(
+            std::string("Buildings"),
+            upgrade->getName(),
+            getPictureNameFromBuilding(upgrade->getType()),
+            upgrade->getDescription(),
+            (
+              upgrade->isFinished() ?
               std::string("done") :
               computeDisplayTime(upgrade->getRemainingTime())
-            ) :
-            std::string("")
+            )
           );
+        }
+        else {
         }
       }
       catch (const core::PlanetException& e) {
@@ -87,7 +62,31 @@ namespace ogame {
       lock();
 
       try {
+        // Retrieve the building upgrades action and display the first one.
+        const std::vector<core::ResearchUpgradeActionShPtr>& upgrades = account.getResearchUpgrades();
+        bool hasResearchUpgrade = !upgrades.empty();
+        const core::ResearchUpgradeAction* upgrade = nullptr;
+        if (hasResearchUpgrade) {
+          upgrade = upgrades[0u].get();
+        }
 
+        std::cout << "[UPGRADE] Planet " << account.getPlayerName() << " has " << upgrades.size() << " research(es) upgrade(s)" << std::endl;
+
+        if (hasResearchUpgrade) {
+          populatePanel(
+            std::string("Research"),
+            upgrade->getName(),
+            getPictureNameFromResearch(upgrade->getType()),
+            upgrade->getDescription(),
+            (
+              upgrade->isFinished() ?
+              std::string("done") :
+              computeDisplayTime(upgrade->getRemainingTime())
+            )
+          );
+        }
+        else {
+        }
       }
       catch (const core::PlanetException& e) {
         std::cerr << "[DURATION] Could not populate upgrade action container " << getName() << " with research actions for account " << account.getPlayerName() << ":" << std::endl << e.what() << std::endl;
@@ -101,7 +100,31 @@ namespace ogame {
       lock();
 
       try {
+        // Retrieve the building upgrades action and display the first one.
+        const std::vector<core::ShipUpgradeActionShPtr>& upgrades = planet.getShipUpgrades();
+        bool hasShipUpgrade = !upgrades.empty();
+        const core::ShipUpgradeAction* upgrade = nullptr;
+        if (hasShipUpgrade) {
+          upgrade = upgrades[0u].get();
+        }
 
+        std::cout << "[UPGRADE] Planet " << planet.getName() << " has " << upgrades.size() << " shipyard(s) upgrade(s)" << std::endl;
+
+        if (hasShipUpgrade) {
+          populatePanel(
+            std::string("Shipyard"),
+            upgrade->getName(),
+            getPictureNameFromShip(upgrade->getType()),
+            upgrade->getDescription(),
+            (
+              upgrade->isFinished() ?
+              std::string("done") :
+              computeDisplayTime(upgrade->getRemainingTime())
+            )
+          );
+        }
+        else {
+        }
       }
       catch (const core::PlanetException& e) {
         std::cerr << "[DURATION] Could not populate upgrade action container " << getName() << " with shipyard actions for planet " << planet.getName() << ":" << std::endl << e.what() << std::endl;
@@ -122,7 +145,9 @@ namespace ogame {
         std::string("title"),
         std::string("Default upgrade title"),
         SDL_Color{96, 159, 197, SDL_ALPHA_OPAQUE},
-        LabelContainer::Alignment::Center
+        LabelContainer::Alignment::Center,
+        SDL_Color{22, 30, 36, SDL_ALPHA_OPAQUE},
+        false
       );
 
       LabelContainerShPtr upgradeName = createLabel(
@@ -131,7 +156,7 @@ namespace ogame {
         SDL_Color{255, 255, 255, SDL_ALPHA_OPAQUE}
       );
 
-      PictureContainerShPtr picture = ComponentFactory::createPicturePanel(std::string("picture"), std::string("data/img/solar_plant.bmp"));
+      PictureContainerShPtr picture = ComponentFactory::createPicturePanel(std::string("picture"), std::string("data/img/solar_plant_small.bmp"));
 
       LabelContainerShPtr upgradeDescription = createLabel(
         std::string("upgrade_desc"),
@@ -148,7 +173,7 @@ namespace ogame {
       LabelContainerShPtr duration = createLabel(
         std::string("duration_value"),
         std::string("0s"),
-        SDL_Color{255, 255, 255, SDL_ALPHA_OPAQUE}
+        SDL_Color{156, 102, 28, SDL_ALPHA_OPAQUE}
       );
 
       if (title == nullptr ||
@@ -176,6 +201,41 @@ namespace ogame {
 
       setLayout(layout);
     }
+
+    void UpgradeActionContainer::populatePanel(const std::string& upgradeType,
+                                               const std::string& upgradeName,
+                                               const std::string& upgradePicture,
+                                               const std::string& upgradeDesc,
+                                               const std::string& upgradeDuration)
+    {
+      // Update each information.
+      LabelContainer* title = getChild<LabelContainer*>(std::string("title"));
+      if (checkChild(title, "Upgrade action container title")) {
+        title->setText(upgradeType);
+      }
+
+      LabelContainer* name = getChild<LabelContainer*>(std::string("upgrade_name"));
+      if (checkChild(name, "Upgrade action container upgrade name")) {
+        name->setText(upgradeName);
+      }
+
+      PictureContainer* picture = getChild<PictureContainer*>(std::string("picture"));
+      if (checkChild(picture, "Upgrade action container picture")) {
+        picture->setImagePath(upgradePicture);
+      }
+
+      LabelContainer* desc = getChild<LabelContainer*>(std::string("upgrade_desc"));
+      if (checkChild(desc, "Upgrade action container description")) {
+        desc->setText(upgradeDesc);
+      }
+
+      LabelContainer* duration = getChild<LabelContainer*>(std::string("duration_value"));
+      if (checkChild(duration, "Upgrade action container duration")) {
+        duration->setText(upgradeDuration);
+      }
+    }
+
+    void UpgradeActionContainer::clearPanel() {}
 
   }
 }
