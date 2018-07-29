696feb6a8cadef389a5f92ba67d6b7ebe4026132
MaximeMYCS <maxime.rigaud.perso@gmail.com>
2018-07-29 12:00:59 +0200

    Added possibility to set active building, research etc. from abstract action listener without retrieving the data model first

--- src/player/AbstractActionListener.cpp
+++ src/player/AbstractActionListener.cpp
@@ -39,6 +39,22 @@ namespace ogame {
       m_model->setActiveGalaxyCoordinate(coordinate);
     }
 
+    void AbstractActionListener::setActiveResearch(const core::Research::Type& research) const {
+      m_model->setActiveResearch(research);
+    }
+
+    void AbstractActionListener::setActiveBuilding(const core::Building::Type& building) const {
+      m_model->setActiveBuilding(building);
+    }
+
+    void AbstractActionListener::setActiveShip(const core::Ship::Type& ship) const {
+      m_model->setActiveShip(ship);
+    }
+
+    void AbstractActionListener::setActiveDefense(const core::Defense::Type& defense) const {
+      m_model->setActiveDefense(defense);
+    }
+
     void AbstractActionListener::triggerAction(const Action& action) const {
       if (m_model == nullptr) {
         throw DataModelException(std::string("Cannot trigger action, invalid null data model"));
