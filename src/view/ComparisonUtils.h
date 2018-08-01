2909126d57f9056ad7a479f6aeb927dfa40af9b5
MaximeMYCS <prolific-programmer@tuta.com>
2018-08-01 22:29:35 +0200

    Removed double occurrence

--- src/view/ComparisonUtils.h
+++ src/view/ComparisonUtils.h
@@ -7,8 +7,6 @@ namespace ogame {
   namespace view {
     namespace comparison {
 
-      bool doubleEqual(const double& value1, const double& value2);
-
       bool floatEqual(const float& value1, const float& value2);
 
       bool nullVector(const utils::Vector2f& vector);
