ef60bcaa318d052fd263b7166e5bcec97e02b1c8
MaximeMYCS <maxime.rigaud.perso@gmail.com>
2018-08-01 22:29:35 +0200

    Removed double occurrence

--- src/view/ComparisonUtils.cpp
+++ src/view/ComparisonUtils.cpp
@@ -7,18 +7,13 @@ namespace ogame {
   namespace view {
     namespace comparison {
 
-      bool doubleEqual(const double& value1, const double& value2) {
-        double epsilon(5.0f * std::numeric_limits<double>::epsilon());
-        return (std::abs(value1 - value2) < epsilon);
-      }
-
       bool floatEqual(const float& value1, const float& value2) {
         float epsilon(5.0f * std::numeric_limits<float>::epsilon());
         return (std::fabs(value1 - value2) < epsilon);
       }
 
       bool nullVector(const utils::Vector2f& vector) {
-        return (doubleEqual(vector.x(), 0.0f) && doubleEqual(vector.y(), 0.0f));
+        return (floatEqual(vector.x(), 0.0f) && floatEqual(vector.y(), 0.0f));
       }
 
     }
